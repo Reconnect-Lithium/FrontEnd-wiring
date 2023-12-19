b1a116a6feaa42b378024bc5e970c61352474ff8
syifasrh <syifasarah19@gmail.com>
2023-12-19 16:32:49 +0700

    Update profile owner screen

--- Reconnect/src/screens/Profile.jsx
+++ Reconnect/src/screens/Profile.jsx
@@ -1,41 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   View,
   Text,
   Image,
   TouchableOpacity,
   StyleSheet,
-  ScrollView,
+  TextInput,
   SafeAreaView,
+  Alert,
 } from "react-native";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import { Gallery } from "../components/Gallery";
 import { History } from "../components/History";
 import { TopBar } from "../components/TopBar";
+import * as ImagePicker from "expo-image-picker";
 
 const Tab = createMaterialTopTabNavigator();
 
 export const Profile = () => {
+  const [profilePhoto, setProfilePhoto] = useState(
+    "https://via.placeholder.com/150"
+  );
+  const [name, setName] = useState("John Doe");
+  const [desc, setDesc] = useState(
+    "Coffee enthusiast, traveler, photographer."
+  );
+  const [address, setAddress] = useState("123 Coffee Street, Java Island");
+  const [editing, setEditing] = useState(false);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
+
+    if (!result.cancelled) {
+      setProfilePhoto(result.uri);
+    }
+  };
+
   return (
     <SafeAreaView style={styles.safeArea}>
-      {/* Konten header profil */}
       <TopBar />
       <View style={styles.headerContainer}>
-        <Image
-          source={{ uri: "https://via.placeholder.com/150" }}
-          style={styles.profilePhoto}
-        />
-        <Text style={styles.name}>John Doe</Text>
-        <Text style={styles.desc}>
-          Coffee enthusiast, traveler, photographer.
-        </Text>
-        <Text style={styles.address}>123 Coffee Street, Java Island</Text>
-        <TouchableOpacity style={styles.mapButton}>
-          <Text style={styles.mapButtonText}>Show on Map</Text>
+        <TouchableOpacity onPress={pickImage}>
+          <Image source={{ uri: profilePhoto }} style={styles.profilePhoto} />
+        </TouchableOpacity>
+        {editing ? (
+          <TextInput style={styles.input} value={name} onChangeText={setName} />
+        ) : (
+          <Text style={styles.name}>{name}</Text>
+        )}
+        {editing ? (
+          <TextInput style={styles.input} value={desc} onChangeText={setDesc} />
+        ) : (
+          <Text style={styles.desc}>{desc}</Text>
+        )}
+        {editing ? (
+          <TextInput
+            style={styles.input}
+            value={address}
+            onChangeText={setAddress}
+          />
+        ) : (
+          <Text style={styles.address}>{address}</Text>
+        )}
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => setEditing(!editing)}
+        >
+          <Text style={styles.buttonText}>
+            {editing ? "Save Changes" : "Edit Profile"}
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buttonMap}>
+          <Text style={styles.buttonText}>Show on Map</Text>
         </TouchableOpacity>
       </View>
 
-      {/* Tab Navigator */}
       <Tab.Navigator>
         <Tab.Screen name="Gallery" component={Gallery} />
         <Tab.Screen name="History" component={History} />
@@ -53,10 +97,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     paddingVertical: 20,
   },
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
   profilePhoto: {
     width: 120,
     height: 120,
@@ -66,10 +106,6 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     borderWidth: 3,
     borderColor: "#ddd",
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 4 },
-    shadowOpacity: 0.3,
-    shadowRadius: 4,
   },
   name: {
     fontSize: 22,
@@ -88,29 +124,33 @@ const styles = StyleSheet.create({
     color: "#333",
     marginBottom: 20,
   },
-  mapButton: {
-    backgroundColor: "#5E17EB",
-    padding: 10,
-    borderRadius: 20,
+  button: {
+    backgroundColor: "grey",
+    padding: 12,
+    borderRadius: 25,
     alignItems: "center",
-    marginHorizontal: 100,
-    marginBottom: 20,
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 1 },
-    shadowOpacity: 0.8,
-    shadowRadius: 2,
+    marginHorizontal: 50,
+    marginBottom: 10,
   },
-  mapButtonText: {
+  buttonMap: {
+    backgroundColor: "#5E17EB",
+    padding: 12,
+    borderRadius: 25,
+    alignItems: "center",
+    marginHorizontal: 50,
+    marginBottom: 10,
+  },
+  buttonText: {
     color: "#fff",
     fontSize: 16,
   },
-  tabContainer: {
-    flex: 1,
-  },
-  tabContent: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 16,
+  input: {
+    width: "80%",
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "#ddd",
+    borderRadius: 10,
+    marginBottom: 10,
+    backgroundColor: "#f9f9f9",
   },
 });
