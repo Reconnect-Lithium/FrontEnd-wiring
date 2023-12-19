afb7617b7a891862f5731e7580cf3c7f24d87171
syifasrh <syifasarah19@gmail.com>
2023-12-19 13:26:49 +0700

    Update filter component

--- Reconnect/src/components/Filter.jsx
+++ Reconnect/src/components/Filter.jsx
@@ -1,39 +1,78 @@
 import React, { useState } from "react";
-import { View, TextInput, StyleSheet } from "react-native";
+import { View, StyleSheet, TouchableOpacity, Text, ScrollView } from "react-native";
 
 export const Filter = ({ onSelectFilter }) => {
-    const [searchTerm, setSearchTerm] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState("all");
 
-    return (
-        <View style={styles.container}>
-            <TextInput
-                style={styles.searchBar}
-                placeholder="Search Event..."
-                value={searchTerm}
-                onChangeText={text => setSearchTerm(text)}
-                onSubmitEditing={() => onSelectFilter(searchTerm)}
-            />
-        </View>
-    );
+  const categories = [
+    { label: "All", value: "all" },
+    { label: "Music", value: "music" },
+    { label: "Stand Up Comedy", value: "standUpComedy" },
+    { label: "Performance", value: "performance" },
+    { label: "Competitions", value: "competitions" },
+  ];
+
+  const handleCategoryChange = (category) => {
+    setSelectedCategory(category);
+    onSelectFilter(category);
+  };
+
+  return (
+    <View style={styles.container}>
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.buttonsContainer}
+      >
+        {categories.map((category) => (
+          <TouchableOpacity
+            key={category.value}
+            style={[
+              styles.button,
+              selectedCategory === category.value && styles.selectedButton,
+            ]}
+            onPress={() => handleCategoryChange(category.value)}
+          >
+            <Text
+              style={[
+                styles.buttonText,
+                selectedCategory === category.value && styles.selectedButtonText,
+              ]}
+            >
+              {category.label}
+            </Text>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        padding: 10,
-        backgroundColor: '#fff',
-    },
-    searchBar: {
-        height: 40,
-        paddingHorizontal: 10,
-        borderRadius: 10,
-        backgroundColor: '#fff',
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        elevation: 5,
-    },
+  container: {
+    paddingVertical: 10,
+  },
+  buttonsContainer: {
+    paddingHorizontal: 10,
+  },
+  button: {
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    borderRadius: 20,
+    backgroundColor: "#eee",
+    marginRight: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  selectedButton: {
+    backgroundColor: "#5E17EB",
+  },
+  buttonText: {
+    fontSize: 16,
+    color: "#333",
+    textAlign: 'center',
+  },
+  selectedButtonText: {
+    color: "#fff",
+  },
 });
